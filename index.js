3a6000391d876f95fcab69465c52fa2c648c5b88
onikunai <greentea5461@gmail.com>
2020-02-18 01:34:11 +0900

    start

--- index.js
+++ index.js
@@ -1,95 +1,77 @@
-//諸々の設定
-var channel_access_token = 'S8OSNXHq8fVoYQWCQiMaR7BKi6PDWNj7jOQoKGCERVaIdMzVrUKzFyWrZn3ajcguROeqyB64DM9AspzpySOSanvEXuP1vk5JhHWs4Jywhj0WEj1nl0VqJjrePzcZh8lskqWCBdFdR2qt+Qpi66cAEwdB04t89/1O/w1cDnyilFU='
-var user_id = 'greentea5461'
-var line_url = 'https://api.line.me/v2/bot/message/push'
-var openweathermap_url = 'http://api.openweathermap.org/data/2.5/forecast?id=1859171' //東京ID:1850147_神戸ID:1859171
-var openweathermap_appid = '5aaa15dfcaf8ee2525a241aa75daf965'
-var text
-var text_jp = []
-var rain_info
-var final_text
+"use strict";
 
-//毎日AM6:00〜7:00に以下のメソッドを起動
-function weatherforecast() {
-  //openweathermapから東京の天気予報を取得
-  var weatherforecast_finalurl = openweathermap_url + '&APPID=' + openweathermap_appid
-  var response = UrlFetchApp.fetch(weatherforecast_finalurl)
-  var json = [JSON.parse(response.getContentText())]
-  Logger.log(json[0]) //意図した場所の天気が取得できているか確認
+/** 
+ * Import Packages
+ */
+const server = require("express")();
+const bot_express = require("bot-express");
 
-  //天気情報を日本語に変換
-  for (var i = 0; i <= 4; i++) {
-    text = JSON.stringify((json[0].list[3 + i].weather[0].icon))
-    Logger.log(json[0].list[3 + i]) //意図した時間の天気が取得できているか確認
-    start_weatherforecast(text)
-    text_jp[i] = text
-  }
-  //天気情報をline送信用に編集する
-  text_edit()
-  //lineへ送信する
-  weatherforecast_to_line(channel_access_token,user_id,final_text);
-}
+/** 
+ * Middleware Configuration
+ */
+server.listen(process.env.PORT || 3000, () => {
+    console.log("server is running...");
+});
 
-//天気情報を日本語に変換
-function start_weatherforecast(weather) {
-  if (weather == '"01n"' || weather == '"01d"'){
-    text = '快晴'
-  }
-  if (weather == '"02n"' || weather == '"02d"'){
-    text = '晴れ'
-  }
-  if (weather == '"03n"' || weather == '"03d"'){
-    text = '曇り'
-  }
-  if (weather == '"04n"' || weather == '"04d"'){
-    text = '曇り'
-  }
-  if (weather == '"09n"' || weather == '"09d"'){
-    text = '小雨'
-    rain_info = '今日は傘を持ちましょう。\n'
-  }
-  if (weather == '"10n"' || weather == '"10d"'){
-    text = '雨'
-    rain_info = '今日は傘を持ちましょう。\n'
-  }
-  if (weather == '"11n"' || weather == '"11d"'){
-    text = '雷雨'
-    rain_info = '今日は傘を持ちましょう。\n'
-  }
-  if (weather == '"13n"' || weather == '"13d"'){
-    text = '雪'
-    rain_info = '今日は傘を持ちましょう。\n'
-  }
-  if (weather == '"50n"' || weather == '"50d"'){
-    text = '霧'
-  }
-}
+/** 
+ * Mount bot-express
+ */
+server.use("/webhook", bot_express({
+    language: "ja",
+    messenger: {
+        line: {
+            // channel_id: process.env.LINE_BOT_CHANNEL_ID,
+            // channel_secret: process.env.LINE_BOT_CHANNEL_SECRET
+            channel_id: process.env.LINE_CHANNEL_ID,
+            channel_secret: process.env.LINE_CHANNEL_SECRET
+        }
+    },
+    nlu: {
+        type: "dialogflow",
+        options: {
+            project_id: process.env.GOOGLE_PROJECT_ID,
+            client_email: process.env.GOOGLE_CLIENT_EMAIL,
+            private_key: process.env.GOOGLE_PRIVATE_KEY,
+            language: "ja"
+        }
+    },
+    parser: [{
+        type: "dialogflow",
+        options: {
+            project_id: process.env.GOOGLE_PROJECT_ID,
+            client_email: process.env.GOOGLE_CLIENT_EMAIL,
+            private_key: process.env.GOOGLE_PRIVATE_KEY,
+            language: "ja"
+        }
+    }],
+    memory: {
+        type: process.env.MEMORY_TYPE || "memory-cache", // memory-cache | redis
+        retention: Number(process.env.MEMORY_RETENTION),
+        options: {
+            url: process.env.REDIS_URL
+        }
+    },
+    translator: {
+        type: "google",
+        enable_lang_detection: false,
+        enable_translation: false,
+        options: {
+            project_id: process.env.GOOGLE_PROJECT_ID,
+            client_email: process.env.GOOGLE_CLIENT_EMAIL,
+            private_key: process.env.GOOGLE_PRIVATE_KEY
+        }
+    },
+    logger: {
+        type: process.env.LOGGER_TYPE || "stdout", // stdout | firestore
+        options: { // Options for firestore.
+            project_id: process.env.FIREBASE_PROJECT_ID,
+            client_email: process.env.FIREBASE_CLIENT_EMAIL,
+            private_key: process.env.FIREBASE_PRIVATE_KEY,
+        }
+    },
+    skill: {
+        default: process.env.DEFAULT_SKILL
+    }
+}));
 
-//line送信用にテキストを編集
-function text_edit() {
-  final_text = '天気予報です。\n' + rain_info + '\n09:00  ' + text_jp[0] + '\n12:00  ' + text_jp[1] + '\n15:00  ' + text_jp[2] + '\n18:00  '
-              + text_jp[3]+ '\n21:00  ' + text_jp[4] + '\n\n今日も一日頑張りましょう！'
-}
-
-//LINEへ送信
-function weatherforecast_to_line(channel_access_token,user_id,text){
-  if (rain_info == '今日は傘を持ちましょう。\n') {
-    //LINEに取得結果を送る
-    UrlFetchApp.fetch(line_url,{
-      'headers': {
-          'Content-Type': 'application/json; charset=UTF-8',
-          'Authorization': 'Bearer ' + channel_access_token,
-      },
-      'method': 'post',
-      'payload': JSON.stringify({
-        'to': user_id,
-        'messages' : [
-          {
-            'type':'text',
-            'text':final_text,
-          }
-        ]
-      })
-    });
-  }
-}
+module.exports = server;
